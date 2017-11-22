ca384488d13e4f942b2a48476ba66eb047ff16cd
yonghanlee <ldragonfly@snu.ac.kr>
2017-11-22 22:13:24 +0900

    hello.py modified on Local repository

--- hello.py
+++ hello.py
@@ -4,3 +4,6 @@ print("Tell his world")
 print("Tell her world")
 print("Independency test of master")
 print("Independancy Test of Hotfix")
+
+// For command line git tutorial
+// Local repository modification
